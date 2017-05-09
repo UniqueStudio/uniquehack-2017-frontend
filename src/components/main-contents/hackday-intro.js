38ae5f5494a258ae78243c7343d8bbd96828ef33
MartinNey <nielumartin@hotmail.com>
2017-05-09 17:38:12 +0800

    fix url not working

--- src/components/main-contents/hackday-intro.js
+++ src/components/main-contents/hackday-intro.js
@@ -8,13 +8,17 @@ class HackdayIntro extends Component {
         key={index}
         className="paragraph">{par}</p>
     ));
+    const goToNewPage = () => {window.location = this.props.url.link};
     return (
       <section
         id="intro"
         className="hackday-intro">
         <h2>{this.props.title}</h2>
         {content}
-        <a href={this.props.url.link}>{this.props.url.name}</a>
+        <a
+          href={this.props.url.link}
+          onTouchEnd={goToNewPage}
+          onClick={goToNewPage}>{this.props.url.name}</a>
       </section>
     );
   }
